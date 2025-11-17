22c9c304e7a8167e16fc7d79dea824210e2f88b8
gs <gs@xyz.de>
2025-11-17 18:33:12 +0100

    adapted and added tests for ForecastProvider

--- test/forecast_provider_test.dart
+++ test/forecast_provider_test.dart
@@ -7,8 +7,49 @@ void main() {
     var locationProvider = LocationProvider();
     var forecastProvider = ForecastProvider(locationProvider);
 
+    var now = DateTime.now();
+    var currentHour = now.hour;
+
     bool succeeded = await forecastProvider.fetchHourlyForecast();
     expect(succeeded, true);
-    expect(forecastProvider.hourlyForecast.length, 24);
+    expect(forecastProvider.hourlyForecast.length, 48 - currentHour - 1);
+  });
+
+  test('Sydney local time is after Aschaffenburg time', () async {
+    var locationProvider = LocationProvider();
+    var forecastProvider = ForecastProvider(locationProvider);
+
+    bool succeeded = locationProvider.selectLocation("Sydney");
+    expect(succeeded, true);
+    succeeded = await forecastProvider.fetchHourlyForecast();
+    expect(succeeded, true);
+    var sydneyLocalTime = forecastProvider.hourlyForecast[0].localTime;
+
+    succeeded = locationProvider.selectLocation("Aschaffenburg");
+    expect(succeeded, true);
+    succeeded = await forecastProvider.fetchHourlyForecast();
+    expect(succeeded, true);
+    var aschaffenburgLocalTime = forecastProvider.hourlyForecast[0].localTime;
+
+    expect(sydneyLocalTime.isAfter(aschaffenburgLocalTime), true);
+  });
+
+  test('New York local time is before Aschaffenburg time', () async {
+    var locationProvider = LocationProvider();
+    var forecastProvider = ForecastProvider(locationProvider);
+
+    bool succeeded = locationProvider.selectLocation("New York");
+    expect(succeeded, true);
+    succeeded = await forecastProvider.fetchHourlyForecast();
+    expect(succeeded, true);
+    var newYorkLocalTime = forecastProvider.hourlyForecast[0].localTime;
+
+    succeeded = locationProvider.selectLocation("Aschaffenburg");
+    expect(succeeded, true);
+    succeeded = await forecastProvider.fetchHourlyForecast();
+    expect(succeeded, true);
+    var aschaffenburgLocalTime = forecastProvider.hourlyForecast[0].localTime;
+
+    expect(newYorkLocalTime.isBefore(aschaffenburgLocalTime), true);
   });
 }
